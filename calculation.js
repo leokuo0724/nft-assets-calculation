6438416213b6e0219054fb93131296aebfc77581
Leo Kuo <p900372012@gmail.com>
2022-02-19 17:05:38 +0800

    feat: add fixed component rules

--- calculation.js
+++ calculation.js
@@ -11,20 +11,22 @@ await fetch(process.env.SETTINGS_URL)
 // constants
 const MAX_RETRY = 1000
 const TARGET_NUMBER = Infinity
-const EXCEPT_PRIORITY = ['T2', 'T3']
+const EXCEPT_PRIORITIES = ['T2', 'T3']
 
 // generate trait pool
-let pool = {};
-let traitsSet = new Set(); // for checking if all traits are used
+const pool = {};
+const fixedTraitIndex = {};
+const traitsSet = new Set(); // for checking if all traits are used
 for (const asset of assetSettings) {
-  const { id, trait_name, rarity_ratio, priority } = asset;
+  const { id, trait_name, rarity_ratio, priority, fixed_rules } = asset;
 
   if (!rarity_ratio) {
     continue;
   }
-  if (EXCEPT_PRIORITY.includes(priority)) {
+  if (EXCEPT_PRIORITIES.includes(priority)) {
     continue;
   }
+  // create pool array
   if (!pool[trait_name]) {
     pool[trait_name] = [];
   }
@@ -32,34 +34,66 @@ for (const asset of assetSettings) {
     pool[trait_name].push(id);
     traitsSet.add(id);
   }
+  // create trait index
+  fixedTraitIndex[id] = fixed_rules;
 }
 
 // generate assets
-let assetSet = new Set();
+const assetSet = new Set();
 let counter = 0;
 while (assetSet.size < TARGET_NUMBER) {
   let characterTraits = '';
   let retry = true;
 
+  const poolArrays = Object.values(pool);
+  // TODO: exception rules
   while (retry && counter < MAX_RETRY) {
-    for (const idArray of Object.values(pool)) {
+    const addedType = new Set();
+    const needForceAdded = {}
+    for (const idArray of poolArrays) {
+      // check if need to force added
+      const currentType = idArray[0].split('-')[0]
+      if (needForceAdded[currentType]) {
+        characterTraits += needForceAdded[currentType]
+        continue;
+      }
+
       // random pick
       const traitId = idArray[Math.floor(Math.random() * idArray.length)];
-      characterTraits += traitId
+      // check if the trait type has been added
+      const type = traitId.split('-')[0];
+      if (addedType.has(type)) {
+        continue;
+      }
+      // add the trait with id
+      characterTraits += traitId;
+      addedType.add(type);
+      // add force added list
+      const fixedTraitIds =  fixedTraitIndex[traitId];
+      if (fixedTraitIds && fixedTraitIds.length > 0) {
+        fixedTraitIds.forEach(traitId => {
+          const fixedTraitType = traitId.split('-')[0]
+          if (needForceAdded[fixedTraitType]) {
+            throw Error('CONFLICT');
+          }
+          needForceAdded[fixedTraitType] = traitId;
+        });
+      }
     }
     if (!assetSet.has(characterTraits)) {
       retry = false;
     } else {
+      characterTraits = ''
       counter++;
-      console.log('retry', counter);
+      // console.log('retry', counter);
     }
   }
   
   if (counter >= MAX_RETRY) {
-    console.log('max size', assetSet.size);
     break;
   }
 
-  assetSet.add(characterTraits)
+  assetSet.add(characterTraits);
+  console.log(characterTraits);
 }
-console.log(assetSet.size)
+console.log('MAX SIZE:', assetSet.size);
